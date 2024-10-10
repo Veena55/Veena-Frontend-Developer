5bec984fcdea9af9ae1633e70e5d01298f9d2d52
VEENA SURYAPRAKASH RAO <veenasrao5@gmail.com>
2024-10-10 18:51:56 +0530

    searchbox implemented, routes also added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SearchBox from './components/SearchBox'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <section className="">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/search" element={<SearchBox />}></Route>
+        </Routes>
+      </BrowserRouter>
     </section>
   )
 }
